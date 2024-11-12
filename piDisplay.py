c97166c267c6a0d31e3ef08471a7bb2b6bd2e846
Daniel <graydaniel9d9@gmail.com>
2024-11-12 08:41:46 -0500

    Adding a test sh for installs and requirements text.

--- piDisplay.py
+++ piDisplay.py
@@ -40,7 +40,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
-    # flush=True
+    # flush=True # Not working
 )
 
 # RPC connection
