4db67fdd6edd98227b4701c13d08403ac05f7645
Daniel <graydaniel9d9@gmail.com>
2026-03-17 17:13:22 -0400

    Trying clipping fix 1

--- piDisplay.py
+++ piDisplay.py
@@ -342,7 +342,12 @@ def update_price_chart(force_update=False):
                     plot_values = values
 
                 ax.plot(plot_dates, plot_values, color='orange')
+                # Force axes patch clipping
+                ax.patch.set_visible(True)
+                ax.set_clip_path(ax.patch)
+
                 fig.patch.set_facecolor('#191A1A')  # Slightly darker gray for figure background
+                
                 if daily_change >= 0:
                     ax.set_title(f"฿itcoin Price: ${current_price:,.0f} - 24h Change: +{daily_change}%", color='green', loc='left', fontsize=16)
                 else:
