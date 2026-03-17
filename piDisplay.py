a47952579737da80ca7ae02cd82b767a523d8e28
Daniel <graydaniel9d9@gmail.com>
2026-03-17 17:16:49 -0400

    Nuclear clipping fix 3

--- piDisplay.py
+++ piDisplay.py
@@ -326,6 +326,7 @@ def update_price_chart(force_update=False):
                 values = [price[1] for price in prices]
 
                 if viewing_mode == "static":
+                    mask = [(today_midnight_naive <= d <= today_end_naive) for d in plot_dates]
                     # Full midnight-to-midnight EST (00:00-23:59)
                     est = pytz.timezone('US/Eastern')
                     now_est = datetime.now(est)
@@ -341,8 +342,12 @@ def update_price_chart(force_update=False):
                 else:  # rolling - use full data
                     plot_dates = dates
                     plot_values = values
+                    mask = [True] * len(plot_dates)
 
-                ax.plot(plot_dates, plot_values, color='orange')
+                plot_dates_clipped = [d for d, m in zip(plot_dates, mask)]
+                plot_values_clipped = [v for v, m in zip(plot_values, mask)]
+                
+                ax.plot(plot_dates_clipped, plot_values_clipped, color='orange', clip_on=True)
                 # Force axes patch clipping
                 clip_rect = Patch((0, 0), 1, 1, transform=fig.transFigure)
                 for line in ax.lines:
