60f1207430fc902da8572e61beef5fb1f0fd59af
Daniel <graydaniel9d9@gmail.com>
2026-03-17 17:03:45 -0400

    Adjusting right side

--- piDisplay.py
+++ piDisplay.py
@@ -526,8 +526,8 @@ def update_node_table(blockchain_data, network_data, fees):
         # Testing
         # Optimal margins for big figure
         fig.subplots_adjust(
-            left=0.1,      # Y-axis labels
-            right=0.98,     # Edge-to-edge right
+            left=0.09,      # Y-axis labels
+            right=0.95,     # Edge-to-edge right
             top=0.92,       # Title fits
             bottom=0.12,    # Minimal bottom for node info
             wspace=0.2,     # Horizontal subplot spacing
